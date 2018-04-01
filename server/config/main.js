8596b986fd2870ba2b82223be4601386f8cbcaa0
RyanG <vietdungnguyen0412@gmail.com>
2018-04-01 22:19:38 +0700

    chat app with mern

---
+++ server/config/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Secret key for JWT signing and encryption
+  secret: 'super secret passphrase',
+  // Database connection information
+  database: 'mongodb://localhost:27017',
+  // Setting port for server
+  port: 3000,
+  test_port: 3001,
+  test_db: 'mern-starter-test',
+  test_env: 'test'
+};
